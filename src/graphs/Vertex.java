69d5dce9a3c6e6188c4412e73ddd9965d71f0b64
jonnie254 <johnkamanu254@proton.me>
2023-11-19 23:04:11 +0300

    updated the vertex class

--- src/graphs/Vertex.java
+++ src/graphs/Vertex.java
@@ -1,5 +1,16 @@
 package graphs;
 
+import java.util.ArrayList;
+
 public class Vertex {
+	private String Data;
+	private ArrayList<Edge>edges;
+	public Vertex(String inputdata) {
+		this.Data=inputdata;
+		this.edges = new ArrayList<Edge>();
+	}
+	public void addEdge(Vertex endVertex, Integer weight) {
+		
+	}
 
 }
